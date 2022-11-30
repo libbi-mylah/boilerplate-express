20a94e7a8eb84b9967bf9cf7a3c9f74acd18e0cc
Libbi Mylah <95930674+libbi-mylah@users.noreply.github.com>
2022-11-30 14:20:06 -0500

    get a query parameter input from the client

--- myApp.js
+++ myApp.js
@@ -10,7 +10,40 @@ app.get("/", function (req, res) {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
+  // res.json({"message": "Hello json"});
+  var mySecret = process.env["MESSAGE_STYLE"];
+  if (mySecret === "uppercase") {
+    res.json({ message: "HELLO JSON".toUpperCase() });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time,
+    });
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
   res.json({
-    message: "Hello json",
+    echo: word,
+  });
+});
+
+app.get("/name", function (req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  var { first: firstName, last: lastName } = req.query;
+  res.json({
+    name: `${firstName} ${lastName}`,
   });
 });
