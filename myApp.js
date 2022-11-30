ddcef9be3bc1bbd2fcd32b4141014e9b2cbf2125
Libbi Mylah <95930674+libbi-mylah@users.noreply.github.com>
2022-11-30 12:02:13 -0500

    create a route that responds with JSON

--- myApp.js
+++ myApp.js
@@ -8,3 +8,9 @@ app.get("/", function (req, res) {
 });
 
 app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
